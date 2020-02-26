03ff7fec46e391b942bcd1c32b55fe447bb08ac7
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-26 11:57:49 +0100

    create TemperatureService class and consumeMin method

---
+++ src/main/java/com/safa/temperature/api/services/TemperatureService.java
@@ -0,0 +1,24 @@
+package com.safa.temperature.api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.safa.temperature.api.payload.requests.TemperatureRequest;
+import com.safa.temperature.api.payload.responses.TemperatureResponse;
+
+@Service
+public class TemperatureService {
+
+	
+	private final String URL = "http://localhost:8081/api/temperatures" ;
+	@Autowired
+	private RestTemplate restTemplate ; 
+	
+	public TemperatureResponse consumeMin(TemperatureRequest request) {
+		TemperatureResponse response = restTemplate.postForObject(URL+"/min", request, TemperatureResponse.class);
+		return response;
+		
+		
+	}
+}
