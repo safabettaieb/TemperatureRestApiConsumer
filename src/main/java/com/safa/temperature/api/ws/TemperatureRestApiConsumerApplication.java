70bbc9abf4a5ce6d64b63666d0a30e48ae5d17f4
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-26 11:36:22 +0100

    first commit

---
+++ src/main/java/com/safa/temperature/api/ws/TemperatureRestApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.safa.temperature.api.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureRestApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureRestApiConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safa/temperature/api/ws/TemperatureRestApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.temperature.api.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureRestApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
